d572e6592cb2f625a6546f1e257490aebcf690a7
mzmarshall1278 <mzmarshall1278@gmail.com>
2025-05-02 00:18:53 +0100

    basic app setup

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthorModule } from './author/author.module';
+import { S3Module } from './s3/s3.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    AuthModule,
+    AuthorModule,
+    S3Module,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
